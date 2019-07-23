e77a8b5a47859a0e2cd564ef537f2b8249d9c264
Tristin Hewett <39839237+charismental@users.noreply.github.com>
2019-07-23 14:00:17 -0700

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home">
+    <h1>Home</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam neque ea itaque laborum illo sit sint molestiae et explicabo odio perferendis corrupti nostrum, expedita reiciendis suscipit rem natus maiores fuga!</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+};
+</script>
